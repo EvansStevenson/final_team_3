3dd2eb0b16dbc196886550a1a45d0140194de876
masterlink0 <stevensonevans@gmail.com>
2020-11-10 23:02:20 -0700

    Get upload picture working and Polish Add Recipe page

--- controllers/recipe.js
+++ controllers/recipe.js
@@ -1,12 +1,20 @@
 const session = require("express-session");
+const recipe = require("../models/recipe");
 const Recipe = require('../models/recipe');
 
 exports.getHomePage = (req, res) => {
-    res.render('../views/home', {
-        title: 'CSE341 final',
-        path: '/',
-        foods: [], //Added so that home would load without errors. This will eventually mean somehting
-    });
+    Recipe.find()
+        .then(recipe => {
+            res.render('../views/home', {
+                title: 'CSE341 final',
+                path: '/',
+                foods: recipe, //Added so that home would load without errors. This will eventually mean somehting
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.redirect('/500');
+        })
 }
 
 exports.getAddRecipe = (req, res) => {
@@ -41,8 +49,30 @@ exports.postAddRecipe = (req, res) => {
         let currentDirection = req.body['direction' + id];
         instructions.push(currentDirection);
     }
-    let imgPath = "/placeholder";
+    let image = req.file;
+    console.log(image);
+    if (!image) {
+        return res.status(422).render('../views/addrecipe', {
+            title: 'Add Recipe',
+            path: '/recipe/addrecipe',
+            errorMessage: "Attached file is not an image",
+            error: [],
+            submitAdmin: false,
+            oldInput: {
+                servings: req.body.servings,
+                preperationMinutes: req.body.preperationMinutes,
+                cookingMinutes: req.body.cookingMinutes,
+                title: req.body.title,
+                ingredients: ingredients,
+                instructions: instructions,
+                numIngredients: req.body.numIngredients,
+                numDirections: req.body.numDirections,
+            }
+        });
+    }
 
+    const imageUrl = image.path;
+    console.log(image.path)
     const recipe = new Recipe({
         servings: servings,
         preperationMinutes: preperationMinutes,
@@ -50,7 +80,7 @@ exports.postAddRecipe = (req, res) => {
         title: title,
         ingredients: ingredients,
         instructions: instructions,
-        imagePath: imgPath,
+        imagePath: imageUrl,
         creator: req.user
     })
     recipe.save()
