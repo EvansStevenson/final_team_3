5332b9488bb311d7c97c4d3dab992902d4e26d2d
masterlink0 <stevensonevans@gmail.com>
2020-11-27 08:30:28 -0700

    categories update

--- public/js/categories.js
+++ public/js/categories.js
@@ -7,32 +7,56 @@ function getbyId(id) {
     return document.getElementById(id);
 }
 
+function hideAll(){
+    getbyId("chicken").hidden = true;
+    getbyId("chickenbtn").classList.remove('cSelect')
+    getbyId("beef").hidden = true;
+    getbyId("beefbtn").classList.remove('bSelect')
+    getbyId("pork").hidden = true;
+    getbyId("porkbtn").classList.remove('pSelect')
+    getbyId("fish").hidden = true;
+    getbyId("fishbtn").classList.remove('fSelect')
+    getbyId("vegetable").hidden = true;
+    getbyId("vegetablebtn").classList.remove('vSelect')
+    getbyId("vegan").hidden = true;
+    getbyId("veganbtn").classList.remove('veSelect')
+    getbyId("dessert").hidden = true;
+    getbyId("dessertbtn").classList.remove('dSelect')
+}
+
 addbtnlistener("chickenbtn", () => {
-    getbyId("cbuttons").classList.remove('grid');
+    hideAll();
     getbyId("chicken").hidden = false;
+    getbyId("chickenbtn").className += ' cSelect';
 })
 addbtnlistener("beefbtn", () => {
-    getbyId("cbuttons").classList.remove('grid');
+    hideAll();
     getbyId("beef").hidden = false;
+    getbyId("beefbtn").className += ' bSelect';
 })
 addbtnlistener("porkbtn", () => {
-    getbyId("cbuttons").classList.remove('grid');
+    hideAll();
     getbyId("pork").hidden = false;
+    getbyId("porkbtn").className += ' pSelect';
 })
 addbtnlistener("fishbtn", () => {
-    getbyId("cbuttons").classList.remove('grid');
+    hideAll();
     getbyId("fish").hidden = false;
+    getbyId("fishbtn").className += ' fSelect';
 })
 addbtnlistener("vegetablebtn", () => {
-    getbyId("cbuttons").classList.remove('grid');
+    hideAll();
     getbyId("vegetable").hidden = false;
+    getbyId("vegetablebtn").className += ' vSelect';
 })
 addbtnlistener("veganbtn", () => {
-    getbyId("cbuttons").classList.remove('grid');
+    hideAll();
     getbyId("vegan").hidden = false;
+    getbyId("veganbtn").className += ' veSelect';
 })
 addbtnlistener("dessertbtn", () => {
-    getbyId("cbuttons").classList.remove('grid');
+    hideAll();
     getbyId("dessert").hidden = false;
+    getbyId("dessertbtn").className += ' dSelect';
 })
 
